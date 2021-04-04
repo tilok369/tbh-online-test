9b1b5c74d149f06c9d075c17872437a48e0ebe41
Tanjeer <Tanjeer.Haque@asembia.com>
2021-04-05 00:46:32 +0600

    committed

--- Tbh.Online.Test/Tbh.Online.Test.Web/wwwroot/js/site.js
+++ Tbh.Online.Test/Tbh.Online.Test.Web/wwwroot/js/site.js
@@ -5,14 +5,20 @@
 
 var questionService = {
     onCodeChange: function (index) {
-        console.log($('#question-code-' + index).val());
         if ($('#question-code-' + index).val().startsWith('~') && $('#question-code-' + index).val().endsWith('~')) {
             $('#question-code-formatted-' + index).css('font-family', 'monospace');
-            $('#question-code-formatted-' + index).text($('#' + index).val());
+            $('#question-code-formatted-' + index).html(questionService.formatCode($('#question-code-' + index).val()));
         }
         else {
             $('#question-code-formatted-' + index).css('font-family', 'Arial');
-            $('#question-code-formatted-' + index).text($('#question-code-' + index).val());
+            $('#question-code-formatted-' + index).html($('#question-code-' + index).val());
         }
+    },
+
+    formatCode: function (code) {
+        code = code.replace(/~/g, '&nbsp;');
+        code = code.replace(/`/g, '&nbsp;');
+        code = code.replace(/\n/g, '<br />');
+        return code;
     }
 };
